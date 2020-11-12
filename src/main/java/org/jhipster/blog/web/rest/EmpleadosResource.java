9140d606ed311d9b8007c3a85de9538fa2e3f2f3
Ac-04 <carloshernandez.isaias12.2@gmail.com>
2020-11-11 23:49:09 -0800

    :sparkles: el reporte funciona

--- src/main/java/org/jhipster/blog/web/rest/EmpleadosResource.java
+++ src/main/java/org/jhipster/blog/web/rest/EmpleadosResource.java
@@ -1,22 +1,40 @@
 package org.jhipster.blog.web.rest;
 
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import net.sf.jasperreports.export.SimplePdfReportConfiguration;
 import org.jhipster.blog.domain.Empleados;
 import org.jhipster.blog.repository.EmpleadosRepository;
 import org.jhipster.blog.web.rest.errors.BadRequestAlertException;
 
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.ResponseUtil;
+import io.jsonwebtoken.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * REST controller for managing {@link org.jhipster.blog.domain.Empleados}.
@@ -114,5 +132,33 @@ public class EmpleadosResource {
         log.debug("REST request to delete Empleados : {}", id);
         empleadosRepository.deleteById(id);
         return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
+    }
+
+    @GetMapping("/empleados/report")
+    public void empleadosReport(HttpServletResponse response) throws FileNotFoundException, JRException, IOException,IllegalStateException {
+
+        File file  = ResourceUtils.getFile("classpath:empleados.jrxml");
+
+        JasperReport jasperReport  = JasperCompileManager.compileReport(file.getAbsolutePath());
+        System.out.println("doing");
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(empleadosRepository.findAll());
+        HashMap<String,Object> parameters =new HashMap<String, Object>();
+
+        parameters.put("createdBy", "Empleados");
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+         final String filePath = "\\";
+        JasperExportManager.exportReportToPdfFile(jasperPrint, filePath + "Employee_report.pdf");
+
+        response.setContentType("application/x-download");
+        response.setHeader("Content-Disposition", String.format("attachment; filename=\"users.pdf\""));
+
+       try {
+        OutputStream out = response.getOutputStream();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+       } catch (Exception e) {
+           //TODO: handle exception
+       } 
     }
 }
